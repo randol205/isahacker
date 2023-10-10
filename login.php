673933b39d5496c164695c4b203417e152c612b8
Randol <81429286+randol26@users.noreply.github.com>
2023-10-09 21:58:50 -0500

    fine im done

--- login.php
+++ login.php
@@ -1,20 +1,19 @@
-  <?php
-  // Retrieve user input
-  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+<?php
+// Retrieve user input
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
-    
+
     // Validate user credentials
-    // Add your authentication logic here
-    
-    if (/* authentication successful */) {
-      // Redirect to a specified website
-      header('Location: https://example.com');
-      exit;
+    $validUsername = 'randol'; // Set your desired username
+    $validPassword = 'Randol205!'; // Set your desired password
+
+    if ($username === $validUsername && $password === $validPassword) {
+        // Authentication successful
+        header('Location: https://example.com');
+        exit;
     } else {
-      echo 'Invalid username or password. Please try again.';
+        echo 'Invalid username or password. Please try again.';
     }
-  }
-  ?>
-</body>
-</html>
+}
+?>
