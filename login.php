cd87a340f4e34e78a6dd9aba9b1e1b4f8f8fa15c
Randol <81429286+randol26@users.noreply.github.com>
2023-10-09 21:17:05 -0500

    rburh

--- login.php
+++ login.php
@@ -1,23 +1,50 @@
 <?php
-// Simulating user credentials stored in a database
-$validUsername = 'myusername';
-$validPassword = 'mypassword';
+// Establish database connection
 
-// Check if the form is submitted
+// ... (database connection code)
+
+// Check for form submission
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Get the submitted username and password
+    // Get submitted username and password
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Validate the username and password
-    if ($username === $validUsername && $password === $validPassword) {
-        // Authentication successful
-        echo 'Login successful! Redirecting to the dashboard...';
-        // Perform any additional actions or redirect the user to the desired page
-        // e.g., header('Location: dashboard.php');
+    // Validate the submitted data
+    $errors = [];
+
+    // Validate username
+    if (empty($username)) {
+        $errors[] = 'Username is required.';
+    }
+
+    // Validate password
+    if (empty($password)) {
+        $errors[] = 'Password is required.';
+    }
+
+    // If there are no validation errors
+    if (empty($errors)) {
+        // Hash the password
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        // Store the credentials in the database
+        $query = "INSERT INTO users (username, password) VALUES ('$username', '$hashedPassword')";
+        $result = $connection->query($query);
+
+        if ($result) {
+            echo 'Registration successful! You can now log in.';
+        } else {
+            echo 'Error: ' . $connection->error;
+        }
     } else {
-        // Authentication failed
-        echo 'Invalid username or password. Please try again.';
+        // If there are validation errors, display them to the user
+        foreach ($errors as $error) {
+            echo $error . '<br>';
+        }
     }
 }
+
+// Close the database connection
+
+// ... (database connection closing code)
 ?>
